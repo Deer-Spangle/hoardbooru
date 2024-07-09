5f6199ffb51f48cdeda1e42d3f88cce3f96fc807
Deer-Spangle <deer@spangle.org.uk>
2024-07-09 09:24:44 +0100

    Move card_to_hoardbooru_posts() method into Uploader class

--- utils/notion_upload/main.py
+++ utils/notion_upload/main.py
@@ -26,7 +26,7 @@ class Uploader:
         cards = self.list_cards(art_db_resp)
         logger.info(f"Found {len(cards)} cards")
         for card in cards:
-            card_to_hoardbooru_posts(self.notion, self.hoardbooru, self.tag_cache, card)
+            self.card_to_hoardbooru_posts(card)
 
     def list_cards(self, db_resp: dict) -> list[dict]:
         next_token = None
@@ -54,98 +54,92 @@ class Uploader:
             if next_token is None:
                 return results
 
+    def card_to_hoardbooru_posts(self, card: dict) -> dict[int, PostToUpload]:
+        title = card["properties"]["Name"]["title"][0]["plain_text"]
+        logger.info("Processing card: %s", title)
+        logger.info("Card link: %s", card["url"])
+        artist_tags = [
+            HoardbooruTag(artist["name"], HoardbooruTagType.ARTISTS)
+            for artist in card["properties"]["Artist"]["multi_select"]
+        ]
+        character_tags = [
+            HoardbooruTag(char["name"], HoardbooruTagType.CHARACTERS)
+            for char in card["properties"]["Characters"]["multi_select"]
+        ]
+        owner_tags = [
+            HoardbooruTag(owner["name"], HoardbooruTagType.OWNERS)
+            for owner in card["properties"]["Character owners"]["multi_select"]
+        ]
+        group_meta_tags = [HoardbooruTag("tagging:needs_check", HoardbooruTagType.META)]
+        uploaded_to_tags = [
+            HoardbooruTag("uploaded_to:" + site["name"].lower().replace(" ", "_"), HoardbooruTagType.META)
+            for site in card["properties"]["Posted to"]["multi_select"]
+        ]
+        misc_tags = [
+            HoardbooruTag(tag["name"], HoardbooruTagType.DEFAULT) for tag in card["properties"]["Tags"]["multi_select"]
+        ]
+        is_nsfw = card["properties"]["NSFW"]["checkbox"]
+        multiple_version = card["properties"]["Multiple versions/images"]["checkbox"]
+        # Result and progressive initialisation
+        results: dict[int, PostToUpload] = {}
+        parent_id: Optional[int] = None
+        pool_post_ids: list[int] = []
+        logger.info("Uploading WIPs")
+        for wip in card["properties"]["Attachments (WIPs)"]["files"]:
+            if "file" not in wip:
+                logger.debug("Skipping non-file WIP")
+                continue
+            wip_url = wip["file"]["url"]
+            meta_tags = group_meta_tags + [HoardbooruTag("status:wip", HoardbooruTagType.META)]
+            post = PostToUpload(
+                wip_url,
+                artist_tags,
+                character_tags,
+                owner_tags,
+                meta_tags,
+                misc_tags,
+                is_nsfw,
+                parent_id,
+            )
+            post_id = upload_post(self.hoardbooru, self.tag_cache, post, card["url"])
+            results[post_id] = post
+        logger.info("Updating finals")
+        for final in card["properties"]["Final"]["files"]:
+            if "file" not in final:
+                logger.debug("Skipping non-file final")
+                continue
+            final_url = final["file"]["url"]
+            meta_tags = group_meta_tags + uploaded_to_tags + [HoardbooruTag("status:final", HoardbooruTagType.META)]
+            post = PostToUpload(
+                final_url,
+                artist_tags,
+                character_tags,
+                owner_tags,
+                meta_tags,
+                misc_tags,
+                is_nsfw,
+                parent_id,
+            )
+            post_id = upload_post(self.hoardbooru, self.tag_cache, post, card["url"])
 
-def card_to_hoardbooru_posts(
-        notion: Client,
-        hoardbooru: pyszuru.API,
-        tag_cache: TagCache,
-        card: dict
-) -> dict[int, PostToUpload]:
-    title = card["properties"]["Name"]["title"][0]["plain_text"]
-    logger.info("Processing card: %s", title)
-    logger.info("Card link: %s", card["url"])
-    artist_tags = [
-        HoardbooruTag(artist["name"], HoardbooruTagType.ARTISTS)
-        for artist in card["properties"]["Artist"]["multi_select"]
-    ]
-    character_tags = [
-        HoardbooruTag(char["name"], HoardbooruTagType.CHARACTERS)
-        for char in card["properties"]["Characters"]["multi_select"]
-    ]
-    owner_tags = [
-        HoardbooruTag(owner["name"], HoardbooruTagType.OWNERS)
-        for owner in card["properties"]["Character owners"]["multi_select"]
-    ]
-    group_meta_tags = [HoardbooruTag("tagging:needs_check", HoardbooruTagType.META)]
-    uploaded_to_tags = [
-        HoardbooruTag("uploaded_to:" + site["name"].lower().replace(" ", "_"), HoardbooruTagType.META)
-        for site in card["properties"]["Posted to"]["multi_select"]
-    ]
-    misc_tags = [
-        HoardbooruTag(tag["name"], HoardbooruTagType.DEFAULT) for tag in card["properties"]["Tags"]["multi_select"]
-    ]
-    is_nsfw = card["properties"]["NSFW"]["checkbox"]
-    multiple_version = card["properties"]["Multiple versions/images"]["checkbox"]
-    # Result and progressive initialisation
-    results: dict[int, PostToUpload] = {}
-    parent_id: Optional[int] = None
-    pool_post_ids: list[int] = []
-    logger.info("Uploading WIPs")
-    for wip in card["properties"]["Attachments (WIPs)"]["files"]:
-        if "file" not in wip:
-            logger.debug("Skipping non-file WIP")
-            continue
-        wip_url = wip["file"]["url"]
-        meta_tags = group_meta_tags + [HoardbooruTag("status:wip", HoardbooruTagType.META)]
-        post = PostToUpload(
-            wip_url,
-            artist_tags,
-            character_tags,
-            owner_tags,
-            meta_tags,
-            misc_tags,
-            is_nsfw,
-            parent_id,
-        )
-        post_id = upload_post(hoardbooru, tag_cache, post, card["url"])
-        results[post_id] = post
-    logger.info("Updating finals")
-    for final in card["properties"]["Final"]["files"]:
-        if "file" not in final:
-            logger.debug("Skipping non-file final")
-            continue
-        final_url = final["file"]["url"]
-        meta_tags = group_meta_tags + uploaded_to_tags + [HoardbooruTag("status:final", HoardbooruTagType.META)]
-        post = PostToUpload(
-            final_url,
-            artist_tags,
-            character_tags,
-            owner_tags,
-            meta_tags,
-            misc_tags,
-            is_nsfw,
-            parent_id,
-        )
-        post_id = upload_post(hoardbooru, tag_cache, post, card["url"])
-
-        results[post_id] = post
-        if parent_id is None:
-            parent_id = post_id
-            # Set parent for already uploaded posts
-            logger.debug("Setting parent for already uploaded wips")
-            for posted_id in results.keys():
-                if parent_id == posted_id:
-                    continue
-                set_parent_id(hoardbooru, posted_id, parent_id)
-        # Add to pool list
-        pool_post_ids.append(post_id)
-    # Create pool if applicable
-    if multiple_version:
-        # Create pool
-        create_pool(hoardbooru, title, pool_post_ids)
-    mark_card_uploaded(notion, card["id"])
-    logger.info("Completed card: %s", card["url"])
-    return results
+            results[post_id] = post
+            if parent_id is None:
+                parent_id = post_id
+                # Set parent for already uploaded posts
+                logger.debug("Setting parent for already uploaded wips")
+                for posted_id in results.keys():
+                    if parent_id == posted_id:
+                        continue
+                    set_parent_id(self.hoardbooru, posted_id, parent_id)
+            # Add to pool list
+            pool_post_ids.append(post_id)
+        # Create pool if applicable
+        if multiple_version:
+            # Create pool
+            create_pool(self.hoardbooru, title, pool_post_ids)
+        mark_card_uploaded(self.notion, card["id"])
+        logger.info("Completed card: %s", card["url"])
+        return results
 
 
 def main(config: dict) -> None:
